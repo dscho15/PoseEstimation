2fc3d654e95cbf4ba4e9d8caeadf8cb13ec18768
dscho15 <dscho15@student.sdu.dk>
2023-08-05 18:08:52 +0200

    filled lightning model

--- plmodel.py
+++ plmodel.py
@@ -1,26 +1,44 @@
 from typing import Any
+import torch
 from model import UNet
 from pytorch_lightning import LightningModule
 
 
 class SegmentModel(LightningModule):
 
-    def __init__(self, *args: Any, **kwargs: Any):
-        super().__init__(*args, **kwargs)
+    def __init__(self, 
+                 n_classes : int, 
+                 n_pts : int):
 
-        self.model = UNet()
+        super().__init__()
+        
+        self.model = UNet(n_pts, n_classes)
 
     def forward(self, x):
-        pass
+
+        return self.model(x)
 
     def training_step(self, batch, batch_idx):
-        pass
+        
+        x, y = batch
+        y_hat = self.model(x)
+
+        loss = torch.nn.functional.cross_entropy(y_hat, y)
+
+        return loss
 
     def validation_step(self, batch, batch_idx):
-        pass
+        
+        x, y = batch
+        y_hat = self.model(x)
 
-    def test_step(self, batch, batch_idx):
-        pass
+        loss = torch.nn.functional.cross_entropy(y_hat, y)
+
+        return loss
 
     def configure_optimizers(self):
-        pass
+
+        opt = torch.optim.AdamW(self.parameters(), lr=1e-3, weight_decay=1e-5)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max=10)
+
+        return [opt], [scheduler]
